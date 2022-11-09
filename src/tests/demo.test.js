f5e7a0a203e14768ba7635069d2c91d74f3e1136
Daniela Liendo <danielaliendo98@gmail.com>
2022-11-09 20:15:13 -0300

    demo test was added

---
+++ src/tests/demo.test.js
@@ -0,0 +1,14 @@
+describe('compare strings', () => {
+    test('both messages are the same!', () => {
+
+        // initialization
+        const message1 = 'Hellow world'
+
+        // stimulus
+        const message2 = message1
+
+        // see if the behavior is as expected
+        expect(message1).toBe(message2)
+ 
+    })
+})
